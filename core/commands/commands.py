1a529331ec3cb558784cc347ec7e82c25c20ef57
oppers <oppy.guseru@gmail.com>
2016-03-09 23:27:43 -0800

    Tidied it up

--- core/commands/commands.py
+++ core/commands/commands.py
@@ -1,23 +1,16 @@
 import discord
 from discord.ext import commands
-import json
-import urllib.parse
-from urllib.request import urlopen
-import requests
-from bs4 import BeautifulSoup
-import wikipedia
 
+#Most of these commands are experimental.
 class Commands():
     def __init__(self, bot):
         self.bot = bot
 
     @commands.command()
-    async def test(self, text1 : str, text2 : str, *, text3 : str): #!google command that utilizes the search module.
-        await self.bot.type()
-        await self.bot.say("{} {} {}".format(text1, text2, text3))
-
-    @commands.command()
     async def calc(self, *, value):
+        """
+        Very terrible calculator system.
+        """
         try:
             result = eval(value)
             await self.bot.type()
@@ -30,25 +23,32 @@ class Commands():
             await self.bot.say("Haha, you're funny.")
 
     @commands.command()
-    async def pingbottest(self):
-        await self.bot.type()
-        await self.bot.say("Test")
-
-    @commands.command()
     async def join_date(self, member : discord.Member):
+        """
+        Returns the join date of a user.
+        """
         await self.bot.say("User `{}` joined on `{}`!".format(member.name, member.joined_at))
 
     @commands.command()
     async def userinfo(self, member : discord.Member):
+        """
+        Shows information about a user.
+        """
         await self.bot.say("```Showing information about {} -\r\nName: {}\r\nID: {}\r\nDiscriminator: {}\r\nStatus: {}\r\nJoined: {}\r\nCurrently Playing: {}\r\nAFK: {}\r\nMuted: {}\r\nDeafened: {}\r\nVoice Muted: {}\r\nSound Muted: {}\r\nAvatar: {}```\r\n{}".format(member.name, member.name, member.id, member.discriminator, member.status, member.joined_at, member.game, member.is_afk, member.mute, member.deaf, member.self_mute, member.self_deaf, member.avatar, member.avatar_url))
 
     @commands.command(pass_context=True)
     async def serverinfo(self, ctx):
+        """
+        Returns the information of the server.
+        """
         server = ctx.message.author.server
         await self.bot.say("```Showing information about {}\r\nName: {}\r\nID: {}\r\nRegion: {}\r\nOwner: {}\r\nIcon: {}```\r\n{}".format(server.name, server.name, server.id, server.region, server.owner, server.icon, server.icon_url))
 
     @commands.command(pass_context=True)
     async def kick(self, ctx, member : discord.Member):
+        """
+        Kicks a user. (You must be the server owner to use this command.)
+        """
         try:
             if ctx.message.channel.is_private:
                 await self.bot.say("You cannot use this command in a PM!")
@@ -61,9 +61,19 @@ class Commands():
         except discord.errors.Forbidden:
             await self.bot.say("Failed to kick user!\r\nThe bot does not have the appropriate permission to kick users!")
 
-    @commands.command(help_attrs="Prefix test")
-    async def command_prefix_test(self):
-        await self.bot.say("Prefix is: {}\r\nHelp: {}" .format(self.bot.command_prefix, self.bot.help_attrs))
+    @commands.command()
+    async def avatar(self, member : discord.Member):
+        """
+        Gets the avatar of a user.
+        """
+        await self.bot.say("Avatar of `{}` is {}".format(member.name, member.avatar_url))
+
+    @commands.command()
+    async def waifu(self, member : discord.Member):
+        """
+        Returns your beloved waifu ;)
+        """
+        await self.bot.say("**!!WAIFU ALERT!!**\r\n{}\r\n**!!WAIFU ALERT!!**".format(member.avatar_url))
 
 def setup(bot):
     bot.add_cog(Commands(bot))
